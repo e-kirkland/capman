fbec8fffd880535ebb978d37e6c5ed80a78bc6b6
Eddie Kirkland <qjg0@cdc.gov>
2021-08-15 15:57:11 -0400

    intial commit

---
+++ app.py
@@ -0,0 +1,365 @@
+# Standard library imports
+import requests
+import re
+import sys, os
+import json
+from os.path import join, dirname
+
+# Third party imports
+from flask import Flask, request, jsonify, Response, make_response
+from flask_cors import CORS
+import checkpointe as check
+import redis
+import slack
+from slackeventsapi import SlackEventAdapter
+from dotenv import load_dotenv
+
+
+# Local imports
+from scripts import api_calls as api
+from scripts import initialize as init
+
+# Instantiating app
+app = Flask(__name__)
+# Enable CORS
+cors = CORS(app)
+
+# Getting tokens from env
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+bot_token = os.environ.get('SLACK_BOT_TOKEN')
+user_token = os.environ.get('SLACK_USER_TOKEN')
+secret = os.environ.get('SLACK_SECRET')
+
+print("TOKENS: ", bot_token, user_token, secret)
+
+# Instantiating slackbot
+slack_event_adapter = SlackEventAdapter(secret,'/slack/events',app)
+client = slack.WebClient(token=bot_token)
+BOT_ID = client.api_call("auth.test")['user_id']
+
+league_users = {
+    "U011KTB2AMQ": 1,
+    "U232RJNTH": 2,
+    "U9CQGJH0S": 4,
+    "UBV333PT8": 6,
+    "U236W3RAM": 7,
+    "U232X98MS": 8,
+    "U9FL1AP2B": 10,
+}
+
+# Recognize files created
+@slack_event_adapter.on('file_shared')
+def file_upload(payload):
+    print("FILE FOUND")
+    event = payload.get('event', {})
+    channel_id = event.get('channel_id')
+    user_id = event.get('user_id')
+    
+    print("EVENT: ", event)
+
+    if event != None and BOT_ID != user_id:
+
+        client.chat_postMessage(channel=channel_id, text='Roster file received. Give me a moment to update the league...')
+
+        file = event['file_id']
+        channel = event['channel_id']
+
+        try:
+        
+            user_client = slack.WebClient(token=user_token)
+            result = user_client.api_call('files.sharedPublicURL', params={'file':file, 'channel':channel})
+            print("RESULT: ", result)
+
+            url = result['file']['url_private_download']
+            print("URL: ", url)
+
+            token = user_token
+            resp = requests.get(url, headers={'Authorization': 'Bearer %s' % token})
+
+            print("RESP: ", resp.headers)
+
+            headers = resp.headers['content-disposition']
+            output = 'output/'
+            fname = output + re.findall("filename=(.*?);", headers)[0].strip("'").strip('"')
+
+            assert not os.path.exists(fname), print("File already exists. Please remove/rename and re-run")
+            out_file = open(fname, mode="wb+")
+            print("FNAME: ", fname)
+            out_file.write(resp.content)
+            out_file.close()
+
+            response = api.reset_salary_data(fname)
+            print("RESPONSE: ", response)
+        
+            client.chat_postMessage(channel=channel_id, text='League updated, you are ready to roll!')
+
+        except Exception as e:
+
+            print(f"Exception: {e}")
+            return e, 500
+
+    return Response(), 200
+
+# Message tester
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text').lower()
+
+    if user_id != None and BOT_ID != user_id:
+        if text=='teams':
+            client.chat_postMessage(channel=channel_id, text='Retrieving team info, just a moment...')
+            api_response = get_teams()
+            client.chat_postMessage(channel=channel_id, text=api_response)
+        elif 'roster' in text:
+            roster_id = league_users[user_id]
+            client.chat_postMessage(channel=channel_id, text='Retrieving your roster, just a moment...')
+            teamname = api.get_team_name(roster_id)
+            client.chat_postMessage(channel=channel_id, text=str(teamname))
+            roster = api.get_my_roster(roster_id)
+            client.chat_postMessage(channel=channel_id, text=str(roster))
+        elif text=='cap':
+            roster_id = league_users[user_id]
+            client.chat_postMessage(channel=channel_id, text='Calculating your cap space, hang on...') 
+            print('USERID: ', user_id)
+            cap = api.get_my_cap(roster_id)
+            client.chat_postMessage(channel=channel_id, text=str(cap))
+        elif text=='users':
+            users = event.get('users')
+            print("Users: ", users)
+        else:
+            return 
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    channel_id = data.get('channel_id')
+    user_id = data.get('user_id')
+    client.chat_postMessage(channel=channel_id, text='Got your slash command')
+    return Response(), 200
+
+@app.route('/roster', methods=['POST'])
+def get_roster():
+    data = request.form
+    print("DATA: ", data)
+    channel_id = data.get('channel_id')
+    user_id = data.get('user_id')
+    text = data.get('text')
+    if len(text)!=0:
+        roster_id = api.get_roster_id(text)
+        client.chat_postMessage(channel=channel_id, text=f'Retrieving the roster, just a moment...')
+        teamname = api.get_team_name(roster_id)
+        client.chat_postMessage(channel=channel_id, text=str(teamname))
+        roster = api.get_my_roster(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": roster
+                                        }
+                                    }
+                                ])
+        return Response(), 200
+    else:
+        roster_id = league_users[user_id]
+        client.chat_postMessage(channel=channel_id, text='Retrieving your roster, just a moment...')
+        teamname = api.get_team_name(roster_id)
+        client.chat_postMessage(channel=channel_id, text=str(teamname))
+        roster = api.get_my_roster(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": roster
+                                        }
+                                    }
+                                ])
+        return Response(), 200
+
+@app.route('/cap', methods=['POST'])
+def get_cap():
+    data = request.form
+    print("DATA: ", data)
+    channel_id = data.get('channel_id')
+    user_id = data.get('user_id')
+    text = data.get('text')
+    if len(text)!=0:
+        roster_id = api.get_roster_id(text)
+        client.chat_postMessage(channel=channel_id, text='Calculating the cap space, hang on...') 
+        print('USERID: ', user_id)
+        teamname = api.get_team_cap(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": str(teamname)
+                                        }
+                                    }
+                                ])
+        cap = api.get_my_cap(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": str(cap)
+                                        }
+                                    }
+                                ])
+    else:
+        roster_id = league_users[user_id]
+        client.chat_postMessage(channel=channel_id, text='Calculating your cap space, hang on...') 
+        print('USERID: ', user_id)
+        teamname = api.get_team_cap(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": teamname
+                                        }
+                                    }
+                                ])
+        cap = api.get_my_cap(roster_id)
+        client.chat_postMessage(channel=channel_id, 
+                                text='',
+                                blocks=[
+                                    {
+                                        "type": "section",
+                                        "text": {
+                                            "type": "mrkdwn",
+                                            "text": str(cap)
+                                        }
+                                    }
+                                ])
+
+@app.route('/salary-reset', methods=['POST'])
+def reset_salaries():
+    data = request.form
+    channel_id = data.get('channel_id')
+    user_id = data.get('user_id')
+    client.chat_postMessage(channel=channel_id, text='Looks like you want to revise the league salaries. \nRetrieving league data for you to update, just a moment...')
+    salarycsvfilename = api.get_salary_csv()
+
+    print("PWD: ", os.getcwd())
+
+    response = client.files_upload(
+        channels=channel_id,
+        file=salarycsvfilename,
+        title="Current League Status"
+    )
+    print("RESPONSE: ", response)
+
+    client.chat_postMessage(channel=channel_id, text='Here are the current player/roster/salary standings. Edit them and repost here to update the league.')
+
+    return Response(), 200
+
+# Setup league with one slash comman, including leaguID
+@app.route('/initialize', methods=['POST'])
+def initialize_league():
+    data = request.form
+    print("DATA: ", data)
+    channel_id = data.get('channel_id')
+    user_id = data.get('user_id')
+    text = data.get('text')
+
+    # Pull leagueID, salaryCap, rosterMin, rosterMax from text
+    text_list = text.split(' ')
+    leagueID=text_list[0]
+    salaryCap=text_list[1]
+    rosterMin=text_list[2]
+    rosterMax=text_list[3]
+
+    # Setting up league
+    client.chat_postMessage(channel=channel_id, text=f"Let's do this. Setting up your league with id {leagueID}...")
+
+    # Setting league player info from Sleeper
+    try:
+        # Setting up postgresql with schema based on leagueID
+        return_val = init.setup_league(leagueID, salaryCap, rosterMin, rosterMax)
+
+        # Confirming league setup
+        client.chat_postMessage(channel=channel_id, text=f"Your league is set up. Now it's time to fill in salary info.")
+
+        return_val = reset_salaries()
+
+        return return_val, 200
+
+    except Exception as e:
+
+        return e, 500
+
+    
+
+@app.route('/api/test/', methods=['GET'])
+def test():
+    try:
+        return 'SUCCESS', 200
+    except Exception as e:
+        return f'FAIL {e}'
+
+# Set league ID
+@app.route('/api/setupLeague/', methods=['POST'])
+def setup_league():
+
+    leagueID = request.args.get('leagueID')
+
+    print(f"SETTING LEAGUE ID AS {leagueID}")
+    check.start(summary=True, verbose=True, memory=True)
+
+    try:
+        code_json = api.setup_league(leagueID)
+
+        return code_json, 200
+
+    except Exception as e:
+
+        return f"LEAGUE ID SET ERROR: {e} \n Type: {type(e).__name__}", 500
+
+# Get league transactions
+@app.route('/api/getTransactions/', methods=['GET'])
+def get_transactions():
+
+    try:
+
+        transactions = api.get_transactions()
+
+        return str(transactions), 200
+
+    except Exception as e:
+
+        return f"TRANSACTION RETRIEVAL ERROR {e}", 500
+
+
+# Get team info
+@app.route('/api/getTeams/', methods=['GET'])
+def get_teams():
+
+    try:
+
+        teams = api.get_teams()
+
+        return str(teams), 200
+
+    except Exception as e:
+
+        return f"TEAM RETRIEVAL ERROR {e}", 500
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ scripts/api_calls.py
@@ -0,0 +1,387 @@
+# Standard library imports
+from ast import literal_eval
+import json
+import os, sys
+from os.path import join, dirname
+from typing_extensions import ParamSpecArgs
+from pathlib import Path
+
+# Setting path to sleeper_api_wrapper
+module_path = os.path.abspath(os.path.join('../scripts/sleeper_api_wrapper'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+# Third-party imports
+import pandas as pd
+import checkpointe as check
+import datetime as dt
+from sleeper_wrapper import League, Players
+from dotenv import load_dotenv
+
+# Local imports
+from scripts import postgres_tools as pgt
+
+def get_players():
+    try:
+        # Get all player data
+        players = Players()
+        all_players = players.get_all_players()
+
+        # Convert json to dataframe
+        playerdf = pd.DataFrame.from_dict(all_players, orient='index')
+
+        # Format playerdf for postgres upload
+        playerdf['player'] = playerdf['full_name']
+        keepcols = ['player_id', 'player', 'position', 'team']
+        playerdf = playerdf[keepcols]
+
+        # Limiting to only fantasy-relevant players
+        offensepos = ['WR','RB','TE','QB','K','DEF']
+        playerdf = playerdf[playerdf['position'].isin(offensepos)]
+
+        # Storing placeholders for salary
+        playerdf['salary'] = 0
+
+        return playerdf
+
+    except Exception as e:
+        return print(e)
+
+def setup_league(leagueID):
+    try:
+
+        # Pulling players roster
+        playerdf = get_players()
+
+        print(playerdf.head())
+
+        # Get all roster data
+        teams = get_teams(leagueID)
+
+        print("TEAMS: ", teams.head())
+
+        # Matching players to rosters based on current teams 
+        playerdf['roster_id'] = playerdf['player_id'].apply(lambda x: match_player_to_roster(x, teams))
+
+        # Getting database name
+        parent_path = dirname(__file__)
+        dotenv_path = join(dirname(parent_path), '.env')
+
+        print("PATH TO LOOKUP: ", dotenv_path)
+        load_dotenv(dotenv_path)
+        db = os.environ.get('POSTGRES_DB')
+
+        # Storing player data to postgres
+        pgt.df_to_postgres(playerdf, db, 'players', method='replace')
+
+        # Get current transactions
+        transactions = get_transactions(leagueID)
+
+        # Store most recent transaction
+        transaction_id = get_most_recent_transaction(transactions)
+
+        return transaction_id
+
+    except Exception as e:
+        return print(e)
+
+def get_transactions(leagueID):
+    try:
+
+        # Instantiate league based on id
+        league = League(leagueID)
+
+        # Blank transactions value to start
+        transactions = []
+
+        # List of possible weeks
+        weeklist = range(18, 0, -1)
+        print("WEEKLIST: ", weeklist)
+
+        while len(transactions)==0:
+            for n in weeklist:
+            
+                print(f"TRYING TRANSACTIONS FOR WEEK {n}")
+                # Try to get transactions for week number
+                transactions = league.get_transactions(n)
+            # If no transactions found, return none
+            break
+
+        return transactions
+
+    except Exception as e:
+
+        return print(e)
+
+def compile_team_data(users, rosters):
+
+    try:
+
+        # # Compile roster data
+        rosterdf = pd.DataFrame(rosters)
+
+        print("ROSTER DF LEN: ", len(rosterdf))
+
+        # Narrowing to relevant fields
+        keepcols = ['roster_id', 'players', 'owner_id']
+        rosterdf = rosterdf[keepcols]
+
+        # Compile user data for each roster
+        userdf = pd.DataFrame(users)
+
+        # Narrowing to relevant fields
+        keepcols = ['user_id', 'display_name']
+        userdf = userdf[keepcols]
+
+        # Compiling both dataframes
+        compiled = rosterdf.merge(userdf, "left", left_on='owner_id', right_on='user_id')
+
+        print("COMPILED DF: ", compiled.head())
+
+        keepcols = ['roster_id', 'display_name', 'players']
+        compiled = compiled[keepcols]
+
+        # Storing columns for salary_total and players_total
+        compiled['salary_total'] = 0
+        compiled['players_total'] = 0
+
+        print("FINAL DF: ", compiled.head())
+
+        return compiled
+
+    except Exception as e:
+
+        return e
+
+def get_teams(leagueID):
+
+    try:
+
+        # Instantiate league based on id
+        league = League(leagueID)
+
+        # Get data on all league users for team metadata
+        users = league.get_users()
+
+        # Get data on all rosters
+        rosters = league.get_rosters()
+
+        print("ROSTERS: ", rosters)
+
+        # Compile team info
+        teamsdf = compile_team_data(users, rosters)
+
+        print("TEAM DF COMPILED: ", teamsdf.head())
+
+
+        # Getting database name
+        parent_path = dirname(__file__)
+        dotenv_path = join(dirname(parent_path), '.env')
+
+        print("PATH TO LOOKUP: ", dotenv_path)
+        load_dotenv(dotenv_path)
+        db = os.environ.get('POSTGRES_DB')
+
+        # Post team data to postgres
+        pgt.df_to_postgres(teamsdf, db, 'rosters', method='replace')
+
+        print("ROSTERS POSTED")
+
+        return teamsdf
+
+    except Exception as e:
+
+        return e
+
+def match_player_to_roster(player, rosters):
+
+    match_id = '999'
+    for index, row in rosters.iterrows():
+        players = row['players']
+        if players != None:
+            if player in players:
+                match_id = row['roster_id']
+            else:
+                pass
+        else:
+            pass
+
+    return match_id
+
+def get_most_recent_transaction(transactions):
+
+    # Get most recent transaction
+    transaction = transactions[0]
+    transactionid = transaction["transaction_id"]
+
+    print("TRANSACTION: ", transactionid)
+
+    return str(transactionid)
+
+def get_my_roster(roster_id):
+
+    # Getting database name
+    parent_path = dirname(__file__)
+    dotenv_path = join(dirname(parent_path), '.env')
+
+    print("PATH TO LOOKUP: ", dotenv_path)
+    load_dotenv(dotenv_path)
+    db = os.environ.get('POSTGRES_DB')
+
+    query = f"""SELECT * FROM players WHERE roster_id='{roster_id}';"""
+    rosterdf = pgt.df_from_postgres(query, db, 'players')
+    rosterdf.sort_values(by=['position', 'salary'], inplace=True)
+
+    rosterdf['salary'] = rosterdf['salary'].apply(lambda x: str(x))
+
+    rosterdf.fillna('0', inplace=True)
+
+    message = """| Position | Player | Team | Salary | \n"""
+
+    for n, row in rosterdf.iterrows():
+        playermessage = '| ' + row['position'] + ' | ' + row['player'] + ' | ' + row['team'] + ' | ' + row['salary'] + ' |\n'
+        message = message + playermessage
+
+    return message
+
+def get_team_name(roster_id):
+
+    # Getting database name
+    parent_path = dirname(__file__)
+    dotenv_path = join(dirname(parent_path), '.env')
+
+    print("PATH TO LOOKUP: ", dotenv_path)
+    load_dotenv(dotenv_path)
+    db = os.environ.get('POSTGRES_DB')
+
+    query = f"""SELECT display_name FROM rosters WHERE roster_id='{roster_id}';"""
+    namedf = pgt.df_from_postgres(query, db, 'rosters')
+    print("NAMEDF: ", namedf.head())
+
+    teamname = namedf['display_name'][0]
+
+    returnstring = f'Current roster for _{teamname}_: \n'
+
+    return returnstring
+
+def get_team_cap(roster_id):
+
+    # Getting database name
+    parent_path = dirname(__file__)
+    dotenv_path = join(dirname(parent_path), '.env')
+
+    print("PATH TO LOOKUP: ", dotenv_path)
+    load_dotenv(dotenv_path)
+    db = os.environ.get('POSTGRES_DB')
+
+    query = f"""SELECT display_name FROM rosters WHERE roster_id='{roster_id}';"""
+    namedf = pgt.df_from_postgres(query, db, 'rosters')
+    print("NAMEDF: ", namedf.head())
+
+    teamname = namedf['display_name'][0]
+
+    returnstring = f'Current cap space for _{teamname}_: \n'
+
+    return returnstring
+
+def get_my_cap(roster_id):
+
+    # Getting database name
+    parent_path = dirname(__file__)
+    dotenv_path = join(dirname(parent_path), '.env')
+
+    print("PATH TO LOOKUP: ", dotenv_path)
+    load_dotenv(dotenv_path)
+    db = os.environ.get('POSTGRES_DB')
+
+    query = f"""SELECT SUM(salary) FROM players WHERE roster_id='{roster_id}';"""
+    capdf = pgt.df_from_postgres(query, db, 'players')
+    print("CAPDF: ", capdf.head())
+
+    currentcap = capdf['sum'][0]
+
+    leaguecap = 200
+
+    available = leaguecap - currentcap
+
+    returnstring = f'Current cap spending is *${str(currentcap)}*. \n\nAvailable cap room is *${str(available)}*.'
+
+    return returnstring
+
+def get_salary_csv():
+
+    # Getting database name
+    parent_path = dirname(__file__)
+    dotenv_path = join(dirname(parent_path), '.env')
+
+    print("PATH TO LOOKUP: ", dotenv_path)
+    load_dotenv(dotenv_path)
+    db = os.environ.get('POSTGRES_DB')
+
+    query = f"""SELECT * FROM players;"""
+    playersdf = pgt.df_from_postgres(query, db, 'players')
+    print("PLAYERSDF: ", playersdf.head())
+    playersdf.to_csv('output/players.csv')
+    print("STORED PLAYERS")
+
+    return 'output/players.csv'
+
+def reset_salary_data(fname):
+
+    try:
+
+        # Open csv as df
+        df = pd.read_csv(fname)
+
+        # Format for push to postgres
+        keepcols = ['player_id', 'player', 'position', 'team', 'salary', 'roster_id']
+        df = df[keepcols]
+
+        # Getting database name
+        parent_path = dirname(__file__)
+        dotenv_path = join(dirname(parent_path), '.env')
+
+        print("PATH TO LOOKUP: ", dotenv_path)
+        load_dotenv(dotenv_path)
+        db = os.environ.get('POSTGRES_DB')
+
+        # Upload to postgres
+        pgt.df_to_postgres(df, db, 'players', method='replace')
+
+        return "SUCCESSFULLY UPDATED LEAGUE"
+
+    except Exception as e:
+
+        return f"FAILED UPLOADING TO POSTGRES: {e}"
+
+def get_roster_id(text):
+
+    # Cleaning text
+    text = text.lower().strip()
+
+    lookupdict = {
+        1: ['eddie', 'kirkland', 'process', 'trust'],
+        2: ['cory', 'draper', 'tampa', 'badger'],
+        3: ['will', 'fortanbary', 'essendon', 'bombers'],
+        4: ['jeff', 'herbst', 'kickers', 'qb'],
+        5: ['isaac', 'wesley', 'cleveland', 'steamers'],
+        6: ['nick', 'nicholas', 'bazemore', 'tech', 'gtech', 'ga'],
+        7: ['ryan', 'atkinson', 'rules', 'sucks'],
+        8: ['chris', 'kirkland', 'acworth', 'eagles', 'lame'],
+        9: ['jeremy', 'hess', 'big', 'home'],
+        10: ['alex', 'aghoian', 'beats', 'ray']
+    }
+
+    roster_id = '9'
+
+    for n in range(1,11):
+        id = n
+        array = lookupdict[n]
+        for word in array:
+            if word in text:
+                roster_id = str(id)
+                break
+            else:
+                pass
+    
+    return str(roster_id)
