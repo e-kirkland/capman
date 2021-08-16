2b06aab3225fa425a41919f7c546a3a9834ee659
Eddie Kirkland <qjg0@cdc.gov>
2021-08-16 06:32:45 -0400

    renaming flask parameters

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from scripts import initialize as init
 
 # Instantiating app
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
+# port = int(os.environ.get("PORT", 5000))
 # Enable CORS
 cors = CORS(app)
 
@@ -370,4 +370,4 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(debug=True, port=3000)
